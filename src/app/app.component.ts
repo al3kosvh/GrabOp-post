7f146bedf245d783992cb69363286746af52735b
Omar Arturo García Zamora <omararturo2009@gmail.com>
2017-09-14 12:12:11 -0400

    Fix layouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,63 @@
-import {Component, Directive} from '@angular/core';
-import {AuthHttpMy} from './services/auth-http';
-import {VOUserExt} from './app-login/vouser';
-import {Observable} from 'rxjs/Observable';
+﻿import { Component, Directive } from '@angular/core';
+import { AuthHttpMy } from './services/auth-http';
+import { VOUserExt } from './app-login/vouser';
+import { Observable } from 'rxjs/Observable';
 //import {trigger, state, style, transition, animate, keyframes, group} from '@angular/animations';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
- /* animations: [
-    trigger('routerAnimationRight', [
-      state('in', style({
-        transform: 'translate3d(0, 0, 0)'
-      })),
-      state('out', style({
-        transform: 'translate3d(100%, 0, 0)'
-      })),
-      transition('in => out', animate('400ms ease-in-out')),
-      transition('out => in', animate('400ms ease-in-out'))
-    ]),
-  ]*/
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    /* animations: [
+       trigger('routerAnimationRight', [
+         state('in', style({
+           transform: 'translate3d(0, 0, 0)'
+         })),
+         state('out', style({
+           transform: 'translate3d(100%, 0, 0)'
+         })),
+         transition('in => out', animate('400ms ease-in-out')),
+         transition('out => in', animate('400ms ease-in-out'))
+       ]),
+     ]*/
 })
 export class AppComponent {
-  title = 'AppComponent ';
-  showMe:boolean = true;
-  isLoggedIn$:Observable<boolean>;
+    title = 'AppComponent ';
+    showMe: boolean = true;
+    isLoggedIn: Observable<boolean>;
 
-  user:VOUserExt;
+    user: VOUserExt;
 
-  state: string = 'out';
+    state: string = 'out';
 
-  constructor( auth:AuthHttpMy){
+    constructor(auth: AuthHttpMy) {
 
-    this.isLoggedIn$ = auth.isLogedIn$;
-    auth.user$.subscribe(user=>{
-      if(!user) return;
-     // this.user = user
-    });
+        this.isLoggedIn = auth.isLogedIn$;
+        auth.user$.subscribe(user => {
+            if (!user) return;
+            // this.user = user
+        });
 
-    // console.log('appp');
-  }
+        // console.log('appp');
+    }
 
- // prepareRouter(outlet){
+    // prepareRouter(outlet){
     //const animation = outlet.activatedRouteData['animation'] || {};
-   // return animation['value'] || null;
+    // return animation['value'] || null;
     // let a = r.activeRoute ? r.activeRoute.config.animations : '';
     // console.log('prepareRouter', a);
     // return a;
     // return r.activeRoute ? r.activeRoute.config.animations : '';
- // }
+    // }
 
-  // prepareRouteTransition(r) {
-  //   const animation = r.activatedRouteData['animation'] || {};
-  //   return animation['value'] || null;
-  //
-  //   // return r.activeRoute ? r.activeRoute.config.animations : '';
-  //
-  //   // return this.state = (this.state === 'out' ? 'in' : 'out');
-  // }
+    // prepareRouteTransition(r) {
+    //   const animation = r.activatedRouteData['animation'] || {};
+    //   return animation['value'] || null;
+    //
+    //   // return r.activeRoute ? r.activeRoute.config.animations : '';
+    //
+    //   // return this.state = (this.state === 'out' ? 'in' : 'out');
+    // }
 
 }
