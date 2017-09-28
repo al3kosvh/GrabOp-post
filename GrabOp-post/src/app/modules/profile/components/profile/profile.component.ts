cc15065896c070b6608353302a44513cb70e8d04
Lisvany Alonso <lisvany.alonso@isofact.com>
2017-09-27 20:25:39 -0400

    unit one person to view profile

--- GrabOp-post/src/app/modules/profile/components/profile/profile.component.ts
+++ GrabOp-post/src/app/modules/profile/components/profile/profile.component.ts
@@ -1,118 +1,90 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { MdDialog, MdDialogRef } from '@angular/material';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {trigger, state, style, transition, animate} from '@angular/animations';
+import {MdDialog, MdDialogRef, MdDialogConfig} from '@angular/material';
 
 // Services
-import { ProfileService } from '../../services/profile.service';
-import { PostService } from '../../../post/services/post.service';
-import { AuthHttpService } from '../../../account/services/auth-http.service';
-import { ConnectionService } from '../../../connection/services/connection.service';
+import {ProfileService} from '../../services/profile.service';
+import {PostService} from '../../../post/services/post.service';
+import {AuthHttpService} from '../../../account/services/auth-http.service';
+import {ConnectionService} from '../../../connection/services/connection.service';
 
-import { VOUserExt } from "../../../account/models/vouser";
-import { VOPost } from '../../../../models/vos';
+import {VOUserExt} from "../../../account/models/vouser";
+import {VOPost} from '../../../../models/vos';
 
-import { ModalPromptComponent } from '../../../shared/components/modal-prompt/modal-prompt.component';
+import {ModalPromptComponent} from '../../../shared/components/modal-prompt/modal-prompt.component';
+import {EditProfileDialogComponent} from './edit/edit-profile-dialog.component';
 
 @Component({
-    selector: 'app-profile',
-    templateUrl: './profile.component.html',
-    styleUrls: ['./profile.component.css'],
-    animations: [
-        trigger('slideInOut', [
-            state('in', style({
-                transform: 'translate3d(0, 0, 0)'
-            })),
-            state('out', style({
-                transform: 'translate3d(100%, 0, 0)'
-            })),
-            transition('in => out', animate('400ms ease-in-out')),
-            transition('out => in', animate('400ms ease-in-out'))
-        ]),
-    ]
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class ProfileComponent implements OnInit {
 
-    myUser: VOUserExt = new VOUserExt();
-    person: VOUserExt = new VOUserExt();
-    profileConnectionsCount: number;
-    id: string;
+  myUser: VOUserExt = new VOUserExt();
+  person: VOUserExt = new VOUserExt();
+  profileConnectionsCount: number;
+  id: string;
+  isMyProfile = false;
 
-    allianceInviteState: string = 'out';
+  allianceInviteState: string = 'out';
 
-    personPosts: VOPost[];
+  personPosts: VOPost[];
+  shortName = "";
+  backgroundPic = "#969696";
 
-    constructor(
-        private userService: AuthHttpService,
-        private route: ActivatedRoute,
-        private profileService: ProfileService,
-        private postService: PostService,
-        private connectionService: ConnectionService,
-        private dialog: MdDialog
-    ) {
-        //this.id = this.route.snapshot.params.id;
-        //profileService.getProfileById(this.id);
+  constructor(private userService: AuthHttpService,
+              private route: ActivatedRoute,
+              private profileService: ProfileService,
+              private postService: PostService,
+              private connectionService: ConnectionService,
+              private dialog: MdDialog) {
 
-        //profileService.person$.subscribe(res => this.person = res);
-        //connectionService.getProfileConnectionsCount(this.id).subscribe(res => {
-        //    this.profileConnectionsCount = res;
-        //    console.log('profileConnectionsCount ', res);
-        //});
-    }
+  }
 
-    ngOnInit() {
-        this.userService.user$.subscribe(res => this.myUser = res);
-        //this.postService.getPersonPosts(this.id);
-        //this.postService.posts$.subscribe(posts => {
-        //    console.log('Person posts', posts);
-        //    this.personPosts = posts;
-        //});
-    }
+  ngOnInit() {
+    this.userService.user$.subscribe(res => this.myUser = res);
 
-    setConnection() {
-        console.log('sender ', this.myUser.id);
-        console.log('receiver ', this.id);
-        let message: string;
+    this.isMyProfile = true;
+    let backgroundPic = document.getElementById('backgroundPicProfile');
 
-        let dialogRef = this.dialog.open(ModalPromptComponent, { data: 'Connection message' });
-        dialogRef.afterClosed().subscribe(msg => {
-            console.log('msg', msg);
-            if (msg) {
-                this.connectionService.setConnection(this.myUser.id, this.id, msg);
-            }
-        });
-        // console.log('this.person.id ', this.person.id);
-        // this.profileService.setConnection(this.myUser.id, this.person.id);
-    }
+    this.profileService.getProfileById("16", [
+      person => {
+        this.person = person;
+        this.shortName = this.person.firstName.toString().trim().charAt(0) + '.' + this.person.lastName.toString().trim().charAt(0);
+        this.backgroundPic = this.person.background_pic ? 'url(https://res.cloudinary.com/al3kosvh/image/upload/t_thumbnail/v1468698749/' + this.person.background_pic + ")": this.backgroundPic;
+        console.log("ProfileComponent person: ", this.person, this.person.background_pic, this.backgroundPic, backgroundPic)
+      },
+      err => {
+        console.log("Error charging profile: ", err)
+      },
+      () => {
+      }
+    ])
+  }
 
-    onClickItem(item: VOPost) {
-        this.postService.setSelectedPost(item);
-    }
+  editProfile(): void {
+    let config: MdDialogConfig = {
+      width: '400px',
+      data: this.person
+    };
+    let dialogRef = this.dialog.open(EditProfileDialogComponent, config);
+  }
 
-    openPromptDialog() {
-        // let msg = prompt('Message', '');
-        // console.log('msg', msg);
-        let dialogRef = this.dialog.open(ModalPromptComponent, { data: 'Message' });
-        dialogRef.afterClosed().subscribe(msg => {
-            console.log('msg', msg);
-            if (msg) {
-
-            }
-        });
-        // this.dialogRef.afterClosed().subscribe(result => {
-        //   if(result) {
-        //     console.log('result confirm', result);
-        //     // do confirmation actions
-        //   }
-        //   this.dialogRef = null;
-        // });
-    }
-
-    inviteCancel(state) {
-        this.allianceInviteState = state;
-    }
-
-    allianceInvite() {
-        this.allianceInviteState = this.allianceInviteState === 'out' ? 'in' : 'out';
-    }
+  allianceInvite() {
+    this.allianceInviteState = this.allianceInviteState === 'out' ? 'in' : 'out';
+  }
 }
