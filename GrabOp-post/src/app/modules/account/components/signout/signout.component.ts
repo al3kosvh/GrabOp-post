80406bc0bcf381e8ad55471ee1c9f25527d8b26b
Eniel Rodríguez Machado <eniel160990@gmail.com>
2017-09-28 12:27:25 -0400

    Improve imports in SignOutComponent

--- GrabOp-post/src/app/modules/account/components/signout/signout.component.ts
+++ GrabOp-post/src/app/modules/account/components/signout/signout.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthHttpService } from '../../services/auth-http.service';
-
 
 @Component({
     template: ''
