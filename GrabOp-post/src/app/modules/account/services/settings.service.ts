00ef2294728874e730b58c2b82439407d39b99d3
Eniel Rodríguez Machado <eniel160990@gmail.com>
2017-09-30 21:47:59 -0400

    Refactor settings service accordingly to the new http service

--- GrabOp-post/src/app/modules/account/services/settings.service.ts
+++ GrabOp-post/src/app/modules/account/services/settings.service.ts
@@ -5,7 +5,7 @@ import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
 
 // Services
-import { AuthHttpService } from '../../account/services/auth-http.service';
+import { HttpService } from '../../account/services/http.service';
 
 import { VOSettings } from '../../../models/vos';
 
@@ -15,21 +15,18 @@ export class SettingsService {
     private url: string;
 
     constructor(
-        private authHttpService: AuthHttpService
+        private http: HttpService
     ) {
         this.url = VOSettings.server + '/profiles/';
     }
 
     public getSettings(accountId: string): Observable<Models.Setting[]> {
-        return this.authHttpService.get(this.url + accountId + '/settings?format=json').map((response: Response) =>
-            response.json())
+        return this.http.get(this.url + accountId + '/settings?format=json')
             .catch((error: any) => Observable.throw(error || 'Server error'));
     }
 
     public toggleSetting(accountId: number, settingId, value: string): Observable<Models.Setting> {
-        return this.authHttpService.post(this.url + accountId + '/settings/' + settingId + '?format=json', { value })
-            .map((response: Response) =>
-                response.json())
+        return this.http.post(this.url + accountId + '/settings/' + settingId + '?format=json', { value })
             .catch((error: any) => Observable.throw(error || 'Server error'));
     }
 
