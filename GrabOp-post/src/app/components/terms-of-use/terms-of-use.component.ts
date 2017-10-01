0102b4ecaca532ca0fe7dab7a0a61461947c5dea
Omar Arturo García Zamora <omararturo2009@gmail.com>
2017-09-30 21:53:04 -0400

    Add Initial Component for Miscellaneous Pages

---
+++ GrabOp-post/src/app/components/terms-of-use/terms-of-use.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'terms-of-use',
+    templateUrl: './terms-of-use.component.html',
+    styleUrls: ['./terms-of-use.component.css']
+})
+export class TermsOfUseComponent {
+    constructor() {
+        
+    }
+}
