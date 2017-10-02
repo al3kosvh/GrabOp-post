5203ae03066f8440df98c63862c2996426684c4b
Omar Arturo García Zamora <omararturo2009@gmail.com>
2017-10-01 22:49:32 -0400

    Add new component for select settings

---
+++ GrabOp-post/src/app/modules/profile/components/settings/setting-select/setting-select.component.ts
@@ -0,0 +1,52 @@
+
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+// Services
+import { SettingsService } from '../../../services/settings.service';
+
+@Component({
+    selector: 'setting-select',
+    templateUrl: './setting-select.component.html',
+    styleUrls: ['./setting-select.component.css']
+})
+export class SettingSelectComponent implements OnInit {
+
+    @Input() setting: Models.Setting;
+    value: string;
+    userId: string;
+    units: string[];
+
+    constructor(
+        private route: ActivatedRoute,
+        private settingsService: SettingsService
+    ) {
+        this.route.params.subscribe((params: Params) => {
+            if (params['id']) {
+                this.userId = params['id'];
+            }
+        })
+    }
+
+    ngOnInit() {
+
+        switch (this.setting.name) {
+            case 'distanceUnit': this.units = ['km']; break;
+            case 'currency': this.units = ['CAD', 'USD']; break;
+            default:
+        }
+
+        if (this.setting.fieldtype == "string") {
+            this.value = this.setting.value;
+        }
+    }
+
+    onChange() {
+
+        this.settingsService.toggleSetting(this.userId, this.setting.settingid, this.value).subscribe(
+            setting => {
+                //console.log(setting);
+            }
+        );
+    }
+}
