977b6cd384549e09bb9a5bc1b6a0b44052eadb09
Eniel Rodríguez Machado <eniel160990@gmail.com>
2017-10-07 15:10:40 -0400

    add form groups and validation to control steps  navigation

--- GrabOp-post/src/app/modules/account/components/signup/dialog/signup-dialog.component.ts
+++ GrabOp-post/src/app/modules/account/components/signup/dialog/signup-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, EventEmitter, Output, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { MdDialog, MdDialogRef } from '@angular/material';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 // Services
 import { AuthenticationService } from '../../../services/authentication.service';
@@ -15,7 +15,11 @@ import { SignUpService } from '../../../services/signup.service';
 export class SignUpDialogComponent implements OnInit {
 
     private user: Models.VOUserExt = { occupation: 1 } as Models.VOUserExt;
-    private userFormGroup: FormGroup;
+
+    private formGroup: FormGroup;
+    private nameFormGroup: FormGroup;
+    private jobFormGroup: FormGroup;
+    private locationFormGroup: FormGroup;
 
     private loading = false;
     private checkingEmail = false;
@@ -30,9 +34,7 @@ export class SignUpDialogComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.userFormGroup = this.formBuilder.group({
-            firstCtrl: ['', Validators.required]
-        });
+        this.buildFormGroups();
     }
 
     checkEmail(email) {
@@ -57,28 +59,46 @@ export class SignUpDialogComponent implements OnInit {
     onSubmit(): void {
         this.loading = true;
         this.errorMessage = '';
-        /*this.signupService.signIn(this.signinData).subscribe(
-            value => {
-                this.dialogRef.close();
-                this.loading = false;
-            },
-            error => {
-                switch (error.status) {
-                    case 0:
-                        this.errorMessage = 'Conection error';
-                        break;
-                    case 401:
-                        this.errorMessage = 'Username or password incorrect';
-                        break;
-                    default:
-                        this.errorMessage = error.statusText;
-                }
-                this.loading = false;
-            }
-        );*/
     }
 
     onClose(): void {
         this.dialogRef.close();
+    }
+
+    get formArray(): AbstractControl | null {
+        return this.formGroup.get('formArray');
+    }
+
+    private buildFormGroups() {
+        this.formGroup = this.formBuilder.group({
+
+            formArray: this.formBuilder.array([
+                this.nameFormGroup,
+                this.jobFormGroup,
+                this.locationFormGroup
+            ])
+        });
+
+        this.nameFormGroup = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            username: ['', Validators.required],
+            primaryEmail: ['', Validators.required],
+            password: ['', Validators.required],
+            confirmPassword: ['', Validators.required],
+        });
+
+        this.jobFormGroup = this.formBuilder.group({
+            jobTitle: ['', Validators.email],
+            occupation: ['', Validators.email],
+            company: ['', Validators.email],
+        });
+
+        this.locationFormGroup = this.formBuilder.group({
+            phoneNumber: ['', Validators.email],
+            country: ['', Validators.email],
+            province: ['', Validators.email],
+            city: ['', Validators.email],
+        });
     }
 }
