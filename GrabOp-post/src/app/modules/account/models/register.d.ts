cbac242f0d145f48261383119d6412a05a179690
Eniel Rodríguez Machado <eniel160990@gmail.com>
2017-10-10 18:35:23 -0400

    refactor register definitions

--- GrabOp-post/src/app/modules/account/models/register.d.ts
+++ GrabOp-post/src/app/modules/account/models/register.d.ts
@@ -1,33 +1,33 @@
 declare module Models {
 
     interface VORegisterParameters {
-        username: string
-        firstName: string
-        lastName: string
-        displayName: string
-        email: string
-        password: string
-        autoLogin?: boolean
-        continue?: string
+        username: string;
+        firstName: string;
+        lastName: string;
+        displayName: string;
+        email: string;
+        password: string;
+        autoLogin?: boolean;
+        continue?: string;
     }
 
     interface SORegisterParameters {
-        UserName: string
-        FirstName: string
-        LastName: string
-        DisplayName: string
-        Email: string
-        Password: string
-        AutoLogin?: boolean
-        Continue: string
+        UserName: string;
+        FirstName: string;
+        LastName: string;
+        DisplayName: string;
+        Email: string;
+        Password: string;
+        AutoLogin?: boolean;
+        Continue: string;
     }
 
     interface RegisterResponse {
-        UserId: string
-        SessionId: string
-        UserName: string
-        ReferrerUrl: string
-        ResponseStatus: Models.SOResponseStatus
-        Meta: any
+        UserId: number;
+        SessionId: string;
+        UserName: string;
+        ReferrerUrl: string;
+        ResponseStatus: Models.SOResponseStatus;
+        Meta: any;
     }
 }
