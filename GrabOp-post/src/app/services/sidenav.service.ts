6a80da39f2e4e0746ab4f502afe67eff0bc7d1c2
Omar Arturo García Zamora <omararturo2009@gmail.com>
2017-10-10 21:49:45 -0400

    Fix animations

--- GrabOp-post/src/app/services/sidenav.service.ts
+++ GrabOp-post/src/app/services/sidenav.service.ts
@@ -13,55 +13,66 @@ import { PostCreateComponent } from '../modules/post/components/post-create/post
 @Injectable()
 export class SidenavService {
 
-    sidenav: MatSidenav;
-    viewContainerRef: ViewContainerRef;
-    data: any;
+  sidenav: MatSidenav;
+  viewContainerRef: ViewContainerRef;
+  data: any;
 
-    constructor(
-        private componentFactoryResolver: ComponentFactoryResolver
-    ) { }
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) { }
 
-    registerSidenav(sidenav: MatSidenav) {
-        this.sidenav = sidenav;
+  registerSidenav(sidenav: MatSidenav) {
+    this.sidenav = sidenav;
+  }
+
+  registerViewContainerRef(viewContainerRef: ViewContainerRef) {
+    this.viewContainerRef = viewContainerRef;
+  }
+
+  open() {
+    this.sidenav.open();
+  }
+
+  close() {
+    this.sidenav.close();
+  }
+
+  inviteToAlliance(person: VOUserExt) {
+    this.viewContainerRef.clear()
+    const factory = this.componentFactoryResolver.resolveComponentFactory(AllianceInviteComponent);
+    const ref = this.viewContainerRef.createComponent(factory);
+    let instance: any = ref.instance;
+    instance.person = person;
+    this.sidenav.open();
+  }
+
+  signUp() {
+    this.viewContainerRef.clear()
+    const factory = this.componentFactoryResolver.resolveComponentFactory(SignUpComponent);
+    const ref = this.viewContainerRef.createComponent(factory);
+    //let instance: any = ref.instance;
+    //instance.person = person;
+    this.sidenav.open();
+  }
+
+  onCreatePost(type: string) {
+    let self = this;
+    setTimeout(function () {
+      self.viewContainerRef.clear();
+      const factory = self.componentFactoryResolver.resolveComponentFactory(PostCreateComponent);
+      const ref = self.viewContainerRef.createComponent(factory);
+      let instance: any = ref.instance;
+      instance.type = type;
+      self.sidenav.open();
+    }, this.getTimerValue());    
+  }
+
+  getTimerValue(): number {
+    let value = 0;
+    if (this.sidenav.opened) {
+      this.sidenav.close();
+      value = 500;
     }
-
-    registerViewContainerRef(viewContainerRef: ViewContainerRef) {
-        this.viewContainerRef = viewContainerRef;
-    }
-
-    open() {
-        this.sidenav.open();
-    }
-
-    close() {
-        this.sidenav.close();
-    }
-
-    inviteToAlliance(person: VOUserExt) {
-        this.viewContainerRef.clear()
-        const factory = this.componentFactoryResolver.resolveComponentFactory(AllianceInviteComponent);
-        const ref = this.viewContainerRef.createComponent(factory);
-        let instance: any = ref.instance;
-        instance.person = person;
-        this.sidenav.open();
-    }
-
-    signUp() {
-        this.viewContainerRef.clear()
-        const factory = this.componentFactoryResolver.resolveComponentFactory(SignUpComponent);
-        const ref = this.viewContainerRef.createComponent(factory);
-        //let instance: any = ref.instance;
-        //instance.person = person;
-        this.sidenav.open();
-    }
-
-    onCreatePost(type: string) {
-        this.viewContainerRef.clear();
-        const factory = this.componentFactoryResolver.resolveComponentFactory(PostCreateComponent);
-        const ref = this.viewContainerRef.createComponent(factory);
-        let instance: any = ref.instance;
-        instance.type = type;
-        this.sidenav.open();
-    }
-
+    return value;
+  }
 }
