512e2cd2d860210356a6b55e158062021617886a
Eniel Rodríguez Machado <eniel160990@gmail.com>
2017-10-20 01:41:42 -0400

    fix and refactor snackbar service

--- GrabOp-post/src/app/modules/shared/services/snackbar.service.ts
+++ GrabOp-post/src/app/modules/shared/services/snackbar.service.ts
@@ -1,24 +1,23 @@
 
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class SnackBarService {
 
+  private subj_notification: Subject<Models.Notification> = new Subject();
+
   constructor(
     private matSnackBar: MatSnackBar
-  ) { }
-
-  show(text: string, action: string, duration?: number) {
-    let defaultDuration = 3000;
-    let config = {
-      duration: duration ? duration : defaultDuration
-    };
-    this.matSnackBar.open(text, action, config);
+  ) {
+    this.subj_notification.subscribe(notification => {
+      matSnackBar.open(notification.message, notification.action, { duration: notification.duration ? notification.duration : 3000 });
+    });
   }
 
-  dismiss() {
-    if (this.matSnackBar._openedSnackBarRef)
-      this.matSnackBar._openedSnackBarRef.dismiss();
+  showMessage(notification: Models.Notification) {
+    this.subj_notification.next(notification);
   }
+
 }
