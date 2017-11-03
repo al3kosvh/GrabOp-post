a8a78b1b182e58ad1f205ce01cc9f0a098bacea1
Eniel Rodríguez Machado <eniel160990@gmail.com>
2017-11-03 15:32:32 -0400

    create alliance models

---
+++ GrabOp-post/src/app/modules/post/models/alliance.d.ts
@@ -0,0 +1,47 @@
+declare module Models {
+    //used to create/update an alliance
+    interface Alliance {
+        serviceId: number;
+        isAllianceOpen: boolean;
+        isAllianceBlackBox: boolean;
+        isAllianceMemberTotalVisible: boolean;
+        allianceMembers: number[];
+    }
+
+    //returned by the api
+    interface AllianceExtended {
+        id: number;
+        isBlackBox: boolean;
+        isOpen: boolean;
+        isMemberTotalVisible: boolean;
+        isActive: boolean;
+        createdDate: string;
+        offerId: number;
+        members: Member[];
+        allianceMembersCount: number;
+    }
+
+    interface Member {
+        allianceId: number;
+        membershipStatus: string;
+        hasTasks: boolean;
+        id: number;
+        type: string;
+        userName: string;
+        primaryEmail: string;
+        displayName: string;
+        phoneNumber: string;
+        profilePic: string;
+        jobTitle: string;
+        company: string;
+        firstName: string;
+        lastName: string;
+        occupation: number;
+        url: string;
+        description: string;
+        offers: number;
+        needs: number;
+        numberOfOpps: number;
+        distance: number
+    }
+}
